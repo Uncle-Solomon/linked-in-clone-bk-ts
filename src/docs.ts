d3952ab25d3e114d7e608a588708c5d086024c64
SOLOMON AMEH <amehsolomon46@gmail.com>
2024-11-06 02:07:12 -0800

    bug: render deployment bug

--- src/docs.ts
+++ src/docs.ts
@@ -1,5 +1,6 @@
 import swaggerJSDoc from "swagger-jsdoc";
 import path from "path";
+import { PORT } from "./utils/config";
 
 // Swagger configuration options
 const options: swaggerJSDoc.Options = {
@@ -11,6 +12,11 @@ const options: swaggerJSDoc.Options = {
       description:
         "The API documentation for Linked-in Clone, developed by Ameh Solomon Onyeke (A.S.O)",
     },
+    servers: [
+      {
+        url: process.env.BASE_URL || `http://localhost:${PORT}`,
+      },
+    ],
   },
 
   apis: [path.resolve(__dirname, "./routes/**/*.ts")],
